6829556b299a6337dbc855a60f594fea3d231d53
DeIrw <zantakuhagi@gmail.com>
2020-11-25 23:19:56 +0700

    Add feature Quick Route Management

---
+++ Android App/JadyTrack/app/src/main/java/com/jady/jadytrack/entity/QuickRouteHistory.java
@@ -0,0 +1,18 @@
+package com.jady.jadytrack.entity;
+
+public class QuickRouteHistory {
+
+    private String nameQuickRoute;
+
+    public QuickRouteHistory(String nameQuickRoute){
+        this.nameQuickRoute = nameQuickRoute;
+    }
+
+    public String getNameQuickRoute() {
+        return nameQuickRoute;
+    }
+
+    public void setNameQuickRoute(String nameQuickRoute) {
+        this.nameQuickRoute = nameQuickRoute;
+    }
+}
